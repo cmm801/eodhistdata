c6fc4a6b8faaab2ba834adc98d22ee10523c3e2f
cmm801 <cmm801@gmail.com>
2023-02-26 14:30:45 +0100

    Do not import the contents of private_constants into constants.py

--- eodhistdata/constants.py
+++ eodhistdata/constants.py
@@ -1,6 +1,4 @@
 """Definitions of important constants."""
 
-from eodhistdata.private_constants import API_TOKEN, BASE_PATH
-
 US_EXCHANGES = ['AMEX', 'BATS', 'NASDAQ', 'NMFQS', 'NYSE', 'NYSE ARCA', 'NYSE MKT',
                 'OTC', 'OTCBB', 'OTCCE', 'OTCGREY', 'OTCMKTS', 'OTCQB', 'OTCQX', 'PINK']
