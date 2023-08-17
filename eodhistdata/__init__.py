3e1c79b40bf08d1fb2d63a26e2c96672315c8013
cmm801 <cmm801@gmail.com>
2023-08-17 14:35:52 +0200

    Import main classes into __init__.py

--- eodhistdata/__init__.py
+++ eodhistdata/__init__.py
@@ -1,2 +1,6 @@
 """ A package containing useful functions for downloading data from EOD Historical Data.
 """
+
+from eodhistdata.constants import *
+from eodhistdata.base import EODDataTypes, EODHelper
+from eodhistdata.fundamental_equity import FundamentalEquitySnapshot, FundamentalEquityTS
